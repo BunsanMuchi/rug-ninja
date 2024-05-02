e55a985164d08a863383df1705cd49b6806f38c8
BunsanMuchi <bunsanmuchi@gmail.com>
2024-05-02 23:08:04 +0200

    Initial ideas, just messing around but turning off pc

--- rug-ninja/projects/rug-ninja/contracts/HelloWorld.algo.ts
+++ rug-ninja/projects/rug-ninja/contracts/HelloWorld.algo.ts
@@ -1,62 +1,42 @@
 import { Contract } from '@algorandfoundation/tealscript';
 
-export class HelloWorld extends Contract {
+export class RugNinja extends Contract {
   /** 
    * Rug Ninja should have three possible states
-   * 1
-   * 
+   * Funding: Form is filled, contract is created and the bonding curve is initialized
+   * Trading: Trading can occur within the bonding curve, this is between ALGO/RugCoin. No tokens are traded, it's all in boxes
+   * Funded: Tokens go to users in accordance to the amt bought, and the initial LP in tman is funded, the contract is considered concluded once everyone claims the tokens
   */
-  /**
-   * Calculates the sum of two numbers
-   *
-   * @param a
-   * @param b
-   * @returns The sum of a and b
-   */
-  private getSum(a: uint64, b: uint64): uint64 {
-    return a + b;
+
+  amtRequested = GlobalStateKey<uint64>();
+  amtMinted = GlobalStateKey<uint64>();
+  tokenID = GlobalStateKey<AssetID>();
+
+
+
+  createApplication(): void {
+    
   }
 
-  /**
-   * Calculates the difference between two numbers
-   *
-   * @param a
-   * @param b
-   * @returns The difference between a and b.
-   */
-  private getDifference(a: uint64, b: uint64): uint64 {
-    return a >= b ? a - b : b - a;
+  deleteApplication(...args: any[]): void {
   }
 
-  /**
-   * A method that takes two numbers and does either addition or subtraction
-   *
-   * @param a The first uint64
-   * @param b The second uint64
-   * @param operation The operation to perform. Can be either 'sum' or 'difference'
-   *
-   * @returns The result of the operation
-   */
-  doMath(a: uint64, b: uint64, operation: string): uint64 {
-    let result: uint64;
 
-    if (operation === 'sum') {
-      result = this.getSum(a, b);
-    } else if (operation === 'difference') {
-      result = this.getDifference(a, b);
-    } else throw Error('Invalid operation');
-
-    return result;
+  private createAsset(): void {
+    this.token.value = sendAssetCreation({
+      configAssetName: 'Orange',
+      configAssetUnitName: 'ORA',
+      configAssetManager: this.app.address,
+      configAssetReserve: this.app.address,
+      configAssetFreeze: Address.zeroAddress,
+      configAssetClawback: Address.zeroAddress,
+      configAssetTotal: TOKEN_SUPPLY,
+      configAssetDecimals: TOKEN_DECIMALS,
+      configAssetURL: IFPS_URL,
+      configAssetMetadataHash: rawBytes(IFPS_HASH),
+      fee: 0,
+      note: NOTE,
+    });
   }
 
-  /**
-   * A demonstration method used in the AlgoKit fullstack template.
-   * Greets the user by name.
-   *
-   * @param name The name of the user to greet.
-   * @returns A greeting message to the user.
-   */
-  hello(name: string): string {
-    return 'Hello, ' + name;
-  }
 }
